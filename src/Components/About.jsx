774bd70f0a4bd0cad2b6f9dc409f822083359338
Janarthanan-Balasubramani <janarthananbalasubramani2024@gmail.com>
2024-04-05 21:22:55 +0530

    image-optimization

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -11,8 +11,6 @@ function About() {
 <div className='flex flex-col justify-center text-white'>
 <h1 className='text-4xl font-bold border-b-4 border-primary mb-5 w-[180px] '>About Me </h1>
 <p  className='pb-5'> Im an Full stack developer with expertise in nest js ,node js , react , typescript, golang </p>
-<p  className='pb-5'> Im an Full stack developer with expertise in nest js ,node js , react , typescript, golang </p>
-<p  className='pb-5'> Im an Full stack developer with expertise in nest js ,node js , react , typescript, golang </p>
 
 </div>
 </div>
